4e911939b8294b542227c144f815a0740666a509
Ayush Bhure <ayushbhure2003@gmail.com>
2025-08-24 20:24:59 +0530

    new changes3

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,15 +2,23 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'test3db',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'Ayush@123',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Supabase on Vercel
+  },
 });
+
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST || 'localhost',
+//   port: process.env.DB_PORT || 5432,
+//   database: process.env.DB_NAME || 'test3db',
+//   user: process.env.DB_USER || 'postgres',
+//   password: process.env.DB_PASSWORD || 'Ayush@123',
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// });
 
 // Test the connection
 pool.on('connect', () => {
